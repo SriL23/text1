d255e7646dea46cae579f9f5c0fda97bcf9f5f8c
SriL23 <141116983+SriL23@users.noreply.github.com>
2023-08-03 12:11:23 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,20 @@
+class person
+{
+string name;
+int age;
+void talk()
+{
+System.out.println("Hello my name is"+name);
+System.out.println("and My age is"+age);
+}
+}
+class Demo
+{
+public static void main(String args[])
+{
+person =new person();
+p.name="RAJU";
+p.age=22;
+p.talk();
+}
+}
